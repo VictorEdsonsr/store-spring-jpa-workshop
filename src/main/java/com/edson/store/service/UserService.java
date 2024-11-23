e41080bbcdcc7d5a6810f27f02621055111269a7
VictorEdsonsr <edvictorsr@gmail.com>
2024-11-23 01:22:43 -0400

    Exception handling - findById

--- src/main/java/com/edson/store/service/UserService.java
+++ src/main/java/com/edson/store/service/UserService.java
@@ -2,6 +2,7 @@ package com.edson.store.service;
 
 import com.edson.store.entities.User;
 import com.edson.store.repositories.UserRepository;
+import com.edson.store.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> userOptional = userRepository.findById(id);
-        return userOptional.get();
+        return userOptional.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
---
+++ src/main/java/com/edson/store/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.edson.store.service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id){
+        super("Resource not found . " + id);
+    }
+}
